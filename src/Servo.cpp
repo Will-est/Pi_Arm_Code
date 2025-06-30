e69f2dadc10910e7cc667778779a404e91318c40
Will-est <rwillpainter@gmail.com>
2025-06-30 21:44:07 +0100

    fixed setAngle to output the right pwm(I think)

--- src/Servo.cpp
+++ src/Servo.cpp
@@ -32,9 +32,16 @@ servo::servo(int pin, int id) : pin(pin), id(id), angle(0)
 void servo::setAngle(float theta)
 {
     // convert radians to Pulse width
-    int pulse = (int) (theta * ( 2000 / 3.14159265359));
+    int pulse = (int) (theta * ( 2500 / 180));
+
+    // clips angles to 0 - 180 (consider adding print statment here to say if we clipped)
+    if (pulse < 0) {
+        pulse = 0;
+    } else if (pulse > 2500) {
+        pulse = 2500;
+    }
     // gpioServo(this->pin, pulse);
-    gpioServo(this->pin, 1000);
+    gpioServo(this->pin, pulse); // Adjusting pulse width to start from 500us
     this->angle += theta;
 }
 
