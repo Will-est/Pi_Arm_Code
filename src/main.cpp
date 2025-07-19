525fd385b16adb587b4c710af63621ab7ff80e75
Will Reger <rwillpainter@gmail.com>
2025-07-19 15:44:24 -0500

    Updated Robot configuration

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,20 @@
 #include "Arm.hpp"
 #include <iostream>
 
-
-
 int main()
 {
     
     // Create three ligaments with specified axes and translation vectors
-    float baseAxis[3] = {0.0f, 0.0f, 1.0f};
+    float baseAxis[3] = {0.827f, 0.561f, 2.5f};
     float baseTranslation[3] = {0.0f, 0.0f, 1.0f};
     Arm::Ligament base(baseAxis, baseTranslation);
     
     float rotatorAxis[3] = {0.0f, 1.0f, 0.0f};
-    float rotatorTranslation[3] = {1.0f, 0.0f, 0.0f};
+    float rotatorTranslation[3] = {2.5f, -0.963f, 0.0f};
     Arm::Ligament rotator(rotatorAxis, rotatorTranslation, nullptr, &base);
 
     float eeAxis[3] = {0.0f, 1.0f, 0.0f};
-    float eeTranslation[3] = {1.0f, 0.0f, 0.0f};
+    float eeTranslation[3] = {1.76f, 0.0f, 0.0f};
     Arm::Ligament ee(eeAxis, eeTranslation, nullptr, &rotator);
 
     // Create the arm with the three ligaments
