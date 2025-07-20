f2b851170c348d68b261d013faed04d1208ef585
Will Reger <rwillpainter@gmail.com>
2025-07-19 21:47:38 -0500

    Fixed(hopefully) radian to pulse conversion.

--- src/Servo.cpp
+++ src/Servo.cpp
@@ -1,5 +1,5 @@
 #include "Servo.hpp"
-#include <pigpio.h>
+// #include <pigpio.h>
 #include <iostream>
 
 bool gpio_init_flag = false;
@@ -9,52 +9,52 @@ using namespace Servo;
 // constructor for servo
 servo::servo(int pin) : pin(pin)
 {
-    if(!gpio_init_flag)
-    {
-        // Initialize GPIO pin for servo control
-        if (gpioInitialise() < 0) {
-            // Handle initialization error
-            std::cerr << "The init code didn't work" << std::endl;
-            throw std::runtime_error("Failed to initialize GPIO");
-            return;
-        }
-        else
-        {
-            std::cout << "The init code worked" << std::endl;
-        }
+    // if(!gpio_init_flag)
+    // {
+    //     // Initialize GPIO pin for servo control
+    //     if (gpioInitialise() < 0) {
+    //         // Handle initialization error
+    //         std::cerr << "The init code didn't work" << std::endl;
+    //         throw std::runtime_error("Failed to initialize GPIO");
+    //         return;
+    //     }
+    //     else
+    //     {
+    //         std::cout << "The init code worked" << std::endl;
+    //     }
 
-        gpio_init_flag = true; // sets flag to prevent gpioInitialize being called later
+    //     gpio_init_flag = true; // sets flag to prevent gpioInitialize being called later
 
-    }
-    gpioSetMode(pin, PI_OUTPUT);
+    // }
+    // gpioSetMode(pin, PI_OUTPUT);
 }
 // constructor for servo with min/max angle limits
 servo::servo(int pin, float min_angle, float max_angle) : pin(pin), lower_joint_limit(min_angle), upper_joint_limit(max_angle)
 {
-    if(!gpio_init_flag)
-    {
-        // Initialize GPIO pin for servo control
-        if (gpioInitialise() < 0) {
-            // Handle initialization error
-            std::cerr << "The init code didn't work" << std::endl;
-            throw std::runtime_error("Failed to initialize GPIO");
-            return;
-        }
-        else
-        {
-            std::cout << "The init code worked" << std::endl;
-        }
+    // if(!gpio_init_flag)
+    // {
+    //     // Initialize GPIO pin for servo control
+    //     if (gpioInitialise() < 0) {
+    //         // Handle initialization error
+    //         std::cerr << "The init code didn't work" << std::endl;
+    //         throw std::runtime_error("Failed to initialize GPIO");
+    //         return;
+    //     }
+    //     else
+    //     {
+    //         std::cout << "The init code worked" << std::endl;
+    //     }
 
-        gpio_init_flag = true; // sets flag to prevent gpioInitialize being called later
+    //     gpio_init_flag = true; // sets flag to prevent gpioInitialize being called later
 
-    }
+    // }
 }
 
 
 void servo::setAngle(float theta) 
 {
     // // convert radians to Pulse width
-    int pulse = (int) (1500 + ((theta/PI)*1000));
+    int pulse = (int) (1500 + ((theta/PI)*2000));
 
     // clips angles to 0 - 180 (consider adding print statment here to say if we clipped)
     if (pulse < 0) {
@@ -62,7 +62,7 @@ void servo::setAngle(float theta)
     } else if (pulse > 2500) {
         pulse = 2500;
     }
-    gpioServo(this->pin, pulse);
+    // gpioServo(this->pin, pulse);
 }
 
 float servo::getMinAngle() {
